94d2282d4dec27dca7331424c1dfc3bc4db52359
Sahil wagh <Sahilwagh142@gmail.com>
2024-03-15 10:35:13 +0530

    Avatar-Doc-Complete

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import Image from 'next/image'
-import ReactNexLogo from './assets/ReactNexusLogo.jpg'
+import ReactNexLogo from './assets/ReactNexusLogo.png'
 const config: DocsThemeConfig = {
   logo: 
-  <span style={{display:'flex',flexDirection:'row',alignItems:'center' ,fontWeight:700,fontSize:'20px'}}>
-    <Image src={ReactNexLogo} alt='React-Nex' width={40} height={40}/>
+  <span style={{display:'flex',flexDirection:'row',alignItems:'center' ,fontWeight:700,fontSize:'20px',gap:4}}>
+    <Image src={ReactNexLogo} alt='React-Nex' width={35} height={35}/>
     React <span style={{color:'#61DAFB'}}>Nex</span>
   </span>,
   project: {
--- utils/component-raw-code/component-raw-code.js
+++ utils/component-raw-code/component-raw-code.js
@@ -10,3 +10,25 @@ const App = () => (
 
 export default App;
 `;
+export const AvatarRawCode = `
+import React from 'react';
+import Alert from '../component-react-nex/Alert/Alert';
+
+const App = () => (
+  <div>
+    <Avatar />
+  </div>
+);
+
+export default App;
+`;
+
+
+export const AvatarSizeCode = `
+  <Avatar size={'2xl'} />
+  <Avatar size={'xl'} />
+  <Avatar size={'lg'} />
+  <Avatar size={'md'} />
+  <Avatar size={'sm'} />
+  <Avatar size={'xs'} />
+`
