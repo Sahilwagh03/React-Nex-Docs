6b6f8926698a103f8221863e561ad12ec89b2261
Sahil wagh <Sahilwagh142@gmail.com>
2024-03-15 16:04:36 +0530

    all component-basic doc complete

--- components/Badge/Badge.jsx
+++ components/Badge/Badge.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Badge = ({className="",children ,varient}) => {
+const Badge = ({className="",children="Badge" ,varient}) => {
   return (
-    <div className={`inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold ${varient ? 'bg-transparent text-black border-gray-300' : 'bg-black text-white'} ${className}`}>
+    <div className={`inline-flex w-fit items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold ${varient=="outline" ? 'bg-transparent text-black border-gray-300' : 'bg-black text-white'} ${className}`}>
       {children}
     </div>
   )
