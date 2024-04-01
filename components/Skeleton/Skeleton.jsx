947680c3d74f410fcdb4a06ede7258732c3a9ca3
Sahil wagh <Sahilwagh142@gmail.com>
2024-04-01 20:24:32 +0530

    skeleton added

--- components/Skeleton/Skeleton.jsx
+++ components/Skeleton/Skeleton.jsx
@@ -1,8 +1,17 @@
+import { useTheme } from 'nextra-theme-docs';
 import React from 'react';
 
 const Skeleton = ({className="min-w-[300px] h-4 rounded-md"}) => {
+  const {theme,resolvedTheme}=useTheme()
+  const getSkeletonClasses = ()=>{
+    if ((theme === 'dark' || resolvedTheme === 'dark')) {
+      return 'bg-[#fafafa1a]';
+    } else {
+      return 'bg-[#18181b]/10';
+    }
+  }
   return (
-    <div className={`animate-pulse bg-[#18181b]/10 ${className}`}></div>
+    <div className={`animate-pulse ${getSkeletonClasses(theme,resolvedTheme)} ${className}`}></div>
   );
 };
 
